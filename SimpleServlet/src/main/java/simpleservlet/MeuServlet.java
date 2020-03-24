7c193ad6dfc5c95171b29e65a7229354fb3090a1
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-03-24 17:04:08 -0300

    Criado classe servlet

---
+++ SimpleServlet/src/main/java/simpleservlet/MeuServlet.java
@@ -0,0 +1,30 @@
+package simpleservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/meuServlet")
+public class MeuServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.write("<html>");
+		writer.write("<body>");
+		writer.write("<h1>");
+		writer.write("Primeiro Servlet :) ");
+		writer.write("</hF1>");
+		writer.write("</body>");
+		writer.write("</html>");
+
+	}
+
+}
